3c2521d70edf87dfec214411255ee5efcb599f9c
kochi <kochi.nakajima@gmail.com>
2018-04-13 23:11:17 -0600

    Not Getting the SPAM image yet.

---
+++ src/MiningUIState.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MiningUIState implements UIState{
+	
+
+	@Override
+	public void addComponents(UIFrame frame) {
+		frame.setLayout(new GridLayout(0, 2));
+		
+		JLabel infoLabel = new JLabel("Mine Spam Coin!");
+		infoLabel.setFont(frame.getStandardizedFont(24));
+		frame.add(infoLabel);
+		
+		int[] keyPair = new KeyPairGenerator().generateNext();
+
+		Icon icon = new ImageIcon("Spam_can.png");
+		JButton mine = new JButton(icon);
+		mine.setPreferredSize(new Dimension(200,190));
+		mine.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Node localNode = new Node(keyPair[0], keyPair[1]);
+					localNode.addToNetwork(Runner.GLOBAL_NETWORK);
+					localNode.startWork();
+					frame.initializeLocalNode(localNode);
+			}
+		});
+				
+		frame.add(mine);
+		
+	}
+
+}
