01988735feaf20b1db273ab7ee9f4cb713b5d282
CaseRegan <cp_regan@coloradocollege.edu>
2018-04-15 21:02:31 -0600

    Even more stuff

--- Desktop/BlockChainz/ShrekCoin/src/FactoryFactory.java
+++ Desktop/BlockChainz/ShrekCoin/src/FactoryFactory.java
@@ -1,10 +1,15 @@
 
 public class FactoryFactory {
 	public static enum FactoryType {
-		FactoryFactory,
+		FactoryFactory, // as in a factory that is factor-y
 	};
 	
-	public static FactoryFactory factoryFactoryFactory() {
-		return new FactoryFactory();
+	public static FactoryFactory factoryFactoryFactory(FactoryFactory.FactoryType factoryType) {
+		switch(factoryType) {
+		case FactoryFactory:
+			return new FactoryFactory();
+		default:
+			return null;
+		}
 	}
 }
