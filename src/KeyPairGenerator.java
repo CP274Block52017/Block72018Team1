0be1ebd246f74f48655463a555efedb6c998e21c
kochi <kochi.nakajima@gmail.com>
2018-04-17 13:16:45 -0600

    KeyPairGen actually makes public/private keys

--- src/KeyPairGenerator.java
+++ src/KeyPairGenerator.java
@@ -2,13 +2,15 @@
 // V V V V V
 public class KeyPairGenerator {
 	private static int keysGenerated = 0;
-	
+
 	public KeyPairGenerator() {
 	}
-	
+
 	public int[] generateNext() {
 		// TODO have Kon implement this
+		NumberMagic number = new NumberMagic();
+		int[] keys = number.generateKeyPair();
 		keysGenerated++;
-		return new int[]{keysGenerated - 1, keysGenerated - 1};
+		return new int[]{keys[0], keys[1]};
 	}
 }
