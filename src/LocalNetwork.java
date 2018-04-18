a3980b4c0b31ea39c542a0f4556f31063ae67dd3
Jia <apple@172.17.1.66>
2018-04-17 23:01:06 -0600

    doc

--- src/LocalNetwork.java
+++ src/LocalNetwork.java
@@ -17,20 +17,34 @@ public class LocalNetwork implements Network {
 		nodes = new ArrayList<Node>();
 	}
 	
+	/**
+	 * get requied zeros for hashing
+	 */
 	public int getRequiredZeros() {
 		return 1;
 	}
 	
+	/**
+	 * adds node to the local network
+	 */
 	public void addNode(Node node) {
 		nodes.add(node);
 	}
 	
+	/**
+	 * broadcasts a transaction among all the nodes
+	 * @param transaction object
+	 */
 	public void broadcastNewTransaction(Transaction transaction) {
 		for(Node node : nodes) {
 			node.processNewTransaction(transaction);
 		}
 	}
 	
+	/**
+	 * broadcasts a block sent by one node to the rest of the nodes
+	 * @param block
+	 */
 	public void broadcastNewBlock(Block block) {
 		System.out.println(this + "is sending a block to all nodes...");
 		for(Node node : nodes) {
